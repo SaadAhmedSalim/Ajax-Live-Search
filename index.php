f969f623688b34380b02b839091cf83d9fb33ba0
SaadSalim <shuvrohossain8@gmail.com>
2020-12-24 18:57:46 +0600

    live search issue fixed

--- index.php
+++ index.php
@@ -3,21 +3,26 @@
     <title>MIM</title>
     <script>
       function usersearchTxt(str) {
-        var xmlhttp = new XMLHttpRequest();
-        xmlhttp.onreadystatechange = function () {
-          if(this.readyState == 4 && this.status == 200){
-            document.getElementById('searchTable').innerHTML = this.responseText;
-            //document.getElementById("searchTable").style.border="1px solid #A5ACB2";
+        if (str.length == 0) {
+          document.getElementById("searchTable").innerHTML = "";
+          return;
+        } else {
+          var xmlhttp = new XMLHttpRequest();
+          xmlhttp.onreadystatechange = function () {
+            if(this.readyState == 4 && this.status == 200){
+              document.getElementById('searchTable').innerHTML = this.responseText;
+              //document.getElementById("searchTable").style.border="1px solid #A5ACB2";
+              }
             }
-          }
-        xmlhttp.open("GET","DBManager.php?search="+str,true);
-        xmlhttp.send();
+          xmlhttp.open("GET","DBManager.php?search="+str,true);
+          xmlhttp.send();
 
+        }
       }
     </script>
   </head>
   <body>
-    <input id="searchBox" autocomplete="off" type="text" name="" value="">
+    <input id="searchBox" type="text" name="" onkeyup="usersearchTxt(this.value)">
     <button type="button" name="button" onclick="usersearchTxt(document.getElementById('searchBox').value);">search</button>
 
     <div id="searchTable">
